1290b867af80f4ac02e1a630f1b48c0d566e26e3
zidan4 <zidanjaffar@gmail.com>
2026-09-16 22:21:01 +0300

    shal

--- app/product/[productId]/page.tsx
+++ app/product/[productId]/page.tsx
@@ -1,3 +1,3 @@
 export default function ProductDetails({ params }: { params: { productId: string } }) {
-  return <h1>this is the details for { param.productId }</h1>
+  return <h1>this is the details for {params.productId}</h1>
 }
